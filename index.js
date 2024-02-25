aa7d72fde5512f80008bc48fb2af16e26b660648
Ali Montgomery <alimontgomery@Alis-MacBook-Pro.local>
2024-02-25 20:59:02 +0000

    Added image resizing

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 const config = require('./config.json')
 const findimage = require('./lib/findimage.js')
 const saveFile = require('./lib/save.js')
+const resizeFile = require('./lib/resize.js')
 const path = require('path');
 const fs = require('fs');
 
-const scrape = async (game) => {
-  const url = await findimage(config.url,game)
+const scrape = async (gamename,romspath) => {
+  const url = await findimage(config.url,gamename)
   if(url){
-  await saveFile('./roms/' + game + '.png',url)
-  console.log('✅ Saved image for ' + game);
+  await saveFile('./roms/' + gamename + '.png',url)
+  await resizeFile(romspath + '/' + gamename + '.png',parseInt(config.imgheight)).then(
+    console.log('✅ Saved image for ' + gamename)
+  )
   }
   else{
-    console.log('🤷‍♀️ No image found for ' + game);
+    console.log('🤷‍♀️ No image found for ' + gamename);
   }
 }
 
 const start = async () => {
   const files = fs.readdirSync('./roms/')
-  files.forEach(file => {
-   scrape(path.parse(file).name);
+  files.forEach(async file => {
+   await scrape(path.parse(file).name,path.resolve('./roms/'));
   })
 }
 
